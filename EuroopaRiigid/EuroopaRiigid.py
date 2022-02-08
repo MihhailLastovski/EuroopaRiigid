69c3fd05f4b5069b5c7aedb64172cf10e05e147a
Mihhail Lastovski <lastovskim@gmail.com>
2022-02-09 00:33:30 +0200

    Add project files.

---
+++ EuroopaRiigid/EuroopaRiigid.py
@@ -0,0 +1,69 @@
+﻿from module1 import*
+from random import*
+from tkinter import*
+from tkinter.messagebox import*
+sonastik=failist_sõnastikusse()
+linnad=[i for i in sonastik.keys()]
+pealinnad=[i for i in sonastik.values()]
+def opt1():
+    for key, value in sonastik.items():
+            print(key+"-"+value)
+def opt2():
+    slovo=input("Введите страну или столицу: ")
+    if slovo in linnad:
+        print(slovo+"-"+sonastik[slovo])
+    elif slovo in pealinnad:
+        print(slovo+"-"+linnad[pealinnad.index(slovo)])
+    else:
+        print(f"Страны или столицы нет в словаре")
+
+
+#3
+#new_word(sonastik)
+#4
+#slovo = input("Введите страну, с ошибкой в написании: ")
+#    if slovo in sonastik:
+#        print(f"Страна и ее столица были удалены")
+#        del sonastik[slovo]
+#        new_word(sonastik)
+#    else:
+#        print("Такой страны в списке нет")
+#5
+#    result=0
+#    for i in range(10):
+#        numbe =randint(1,2)
+#        if number==1:
+#            result=test(result,linnad,pealinnad)                
+#        else:
+#            result=test(result,pealinnad,linnad)
+#    hind=result*100/10
+#    print(f"У тебя {result}/10 баллов")
+#    if hind>=90:
+#        print("Вы получили 5")
+#    elif hind>= 75 and hind <=90:
+#        print("Вы получили 4")
+#    elif hind>= 50 and hind <=75:
+#        print("Вы получили 3")
+#    else:
+#        print("Вы получили 2")
+#6
+#    sound()
+
+aken=Tk()
+aken.geometry("1000x600")
+aken.title("Euroopa riigid")
+aken.configure(bg="white")
+Button(aken,text="Все страны и столицы",heigh=3,width=20,font="Arial 15",fg="black",bg="#adc1ff",relief="ridge",command=opt1).pack()
+Button(aken,text="Поиск стран и столиц",heigh=3,width=20,font="Arial 15",fg="black",bg="#adc1ff",relief="ridge",command=opt2).pack()
+Button(aken,text="Новая страна и столица",heigh=3,width=20,font="Arial 15",fg="black",bg="#adc1ff",relief="ridge").pack()
+Button(aken,text="Исправление ошибок",heigh=3,width=20,font="Arial 15",fg="black",bg="#adc1ff",relief="ridge").pack()
+Button(aken,text="Тест",heigh=3,width=20,font="Arial 15",fg="black",bg="#adc1ff",relief="ridge").pack()
+Button(aken,text="Озвучивание",heigh=3,width=20,font="Arial 15",fg="black",bg="#adc1ff",relief="ridge").pack()
+alam_aken=Toplevel()
+alam_aken.geometry("400x400")
+alam_aken.mainloop()
+aken.mainloop()
+
+
+
+
---
+++ EuroopaRiigid/module1.py
@@ -0,0 +1,49 @@
+﻿from random import*
+import os
+import gtts
+from playsound import playsound
+def failist_sõnastikusse():
+    sonastik={}
+    file = open("Euroopa_riigid.txt","r")
+    for line in file:
+        linn,pealinn=line.strip().split(":")
+        sonastik[linn.strip()]=pealinn.strip()
+    file.close()
+    return sonastik
+
+def new_word(sonastik):
+    slovo=input("Введите страну, которую хотите добавить: ")
+    slovo2=input("Введите столицу, которую хотите добавить: ")
+    with open("Euroopa_riigid.txt","a") as fail:
+        fail.write(slovo+":"+slovo2+"\n")
+    sonastik[slovo]=slovo2
+    print(f'Страна "{slovo}" и её столица "{slovo2}" добавлены')
+
+def test(result,l,l2):
+    slovo=choice(l)
+    otvet=input(f"{slovo}: ")
+    if otvet in l2: 
+        if l2.index(otvet)==l.index(slovo):
+            result+=1
+            print("Правильно")
+    else:
+        print("Неправильно")
+    return result
+
+def sound():
+    """Функция для озвучивания текста, введенного пользователем (работает один раз, потом появляется ошибка)
+    """
+    print("На каком языке будет озвучка слова? rus/eng")
+    valik=input("")
+    if valik=="rus":
+        text=input("Введите слово для озвучки на русском: ")
+        tts=gtts.gTTS(text, lang="ru")
+        tts.save("sound_.mp3")
+        playsound("sound_.mp3")
+    elif valik=="eng":
+        text=input("Введите слово для озвучки на английском: ")
+        tts=gtts.gTTS(text, lang="en")
+        tts.save("wordsound.mp3")
+        playsound("wordsound.mp3")
+    else:
+        print("Вы ввели неправильно")
